849f6cf4ce91c7267d428d4a4d15c0963d01819b
Ben Brittain <ben@brittain.org>
2015-02-12 00:13:49 -0500

    React-router impl

--- js/howfuckedisthet.js
+++ js/howfuckedisthet.js
@@ -1,3 +1,58 @@
 var React = require('react');
+var Router = require('react-router');
+var { Route, RouteHandler, Link } = Router;
 
-React.renderComponent(<h1> Pretty fucked. </h1>, document.body);
+var App = React.createClass({
+    mixins: [ Router.Navigation ],
+    getInitialState: function () {
+        return {
+            lines: [
+            { name: 'Red' },
+            { name: 'Orange' },
+            { name: 'Green' },
+            { name: 'Blue' },
+            { name: 'Silver' }
+            ]
+        };
+    },
+    render: function () {
+        var links = this.state.lines.map(function (line, i) {
+            return (
+                    <li key={i}>
+                        <Link to="line" params={line}>{line.name}</Link>
+                    </li>
+                   );
+        });
+        return (
+                <div className="app">
+                    <ul className="nav">
+                        {links}
+                    </ul>
+                    <div className="view">
+                        <RouteHandler />
+                    </div>
+                </div>
+               );
+    }
+});
+
+var Line = React.createClass({
+    mixins: [ Router.State ],
+    render: function () {
+        return (
+                <div className="Line">
+                    <h1>{this.getParams().name}</h1>
+                </div>
+               );
+    }
+});
+
+var routes = (
+        <Route handler={App}>
+        <Route name="line" path="line/:name " handler={Line}/>
+        </Route>
+        );
+
+Router.run(routes, Router.HistoryLocation, function (Handler) {
+    React.render(<Handler/>, document.getElementById('app'));
+});
